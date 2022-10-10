359d112156a4f0e1d617f74ba9703a1e3515c714
Nobelf <13517042@std.stei.itb.ac.id>
2022-10-10 18:02:18 +0700

    add message handler

--- main.py
+++ main.py
@@ -6,6 +6,34 @@ import sqlite3
 import time
 import threading
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '\xe7\xab\x01\xf9\x18\xbb'
+socketio = SocketIO(app, cors_allowed_origins = '*')
+
 con = sqlite3.connect("qa.sqlite3")
 
 dataframe = pd.read_sql_query("SELECT question, answer FROM qa_pair", con)
+
+last_msg = {}
+
+@socketio.on('connect')
+def welcomeMessage(auth):
+    if auth['token'] == 'password123':
+        send("hello there", broadcast = False)
+    else:
+        send("wrong password", broadcast = False)
+        disconnect()
+
+@socketio.on('message')
+def handleMessage(msg):
+
+    q = get_close_matches(msg, dataframe['question'])
+
+    if q:
+        ans = dataframe.loc[dataframe['question'] == q[0], 'answer'].iloc[0]
+        send(ans, broadcast=False)
+    else:
+        send("sorry, I don't understand what you're saying", broadcast = False)
+
+    print(request)
+    last_msg[request.sid] = time.time()
